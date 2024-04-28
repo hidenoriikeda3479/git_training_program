f5ff4730dfff414defcb8f1dfe638729b28f5d32
LapTop\Ikeda <hidenoriike81@gmail.com>
2024-04-28 14:51:55 +0900

    勉強用コンフリを適用

--- GitTraining/MineForm.cs
+++ GitTraining/MineForm.cs
@@ -65,6 +65,8 @@ namespace WindowsFormsApp1
 
             // 画面をShowDialogで開く
 
+
+            MessageBox.Show("");
             // 画面の入力結果を受け取る
 
             // 入力結果に基づいて入力内容をメイン画面に反映する。
@@ -83,6 +85,7 @@ namespace WindowsFormsApp1
 
             // 画面をShowDialogで開く
 
+            MessageBox.Show("");
             // 画面の入力結果を受け取る
 
             // 入力結果に基づいて入力内容をメイン画面に反映する。
